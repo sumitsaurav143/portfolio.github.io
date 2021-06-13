b0380f1755d0419647c6cd7eb0dde176bf644897
SUMIT SAURAV <52030057+sumitsaurav143@users.noreply.github.com>
2021-06-13 22:00:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer","FrontEnd Designer"],
+        strings: ["Java Developer","Python Developer","Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full Stack Developer","FrontEnd Designer"],
+        strings: ["Java Developer","Python Developer","Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
